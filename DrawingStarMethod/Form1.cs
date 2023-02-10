4f6914a22dfc21a45bb67067b2eb4e7fa269f8e5
JellyJamDev <jamejohn2005@gmail.com>
2023-02-10 09:59:16 -0500

    Part 1 Complete

--- DrawingStarMethod/Form1.cs
+++ DrawingStarMethod/Form1.cs
@@ -12,6 +12,9 @@ namespace DrawingStarMethod
 {
     public partial class Form1 : Form
     {
+        PointF[] points = new PointF[10];
+        Graphics g;
+
         public Form1()
         {
             InitializeComponent();
@@ -27,19 +30,46 @@ namespace DrawingStarMethod
 
             ///float size = Convert.ToSingle(sizeInput.Text); 
             
-            Graphics g = this.CreateGraphics(); //for part 4 this gets moved to the custom methods.
+            g = this.CreateGraphics(); //for part 4 this gets moved to the custom methods.
             Pen blackPen = new Pen(Color.Black);
-            
+            Pen whitePen = new Pen(Color.White, 3);
+
+            points[0] = new PointF(110, 5);
+            points[1] = new PointF(140, 80);
+            points[2] = new PointF(225, 80);
+            points[3] = new PointF(155, 125);
+            points[4] = new PointF(180, 200);
+            points[5] = new PointF(115, 160);
+            points[6] = new PointF(40, 200);
+            points[7] = new PointF(70, 130);
+            points[8] = new PointF(5, 80);
+            points[9] = new PointF(85, 80);
+
+            g.DrawPolygon(whitePen, points);
         }
 
         private void fillButton_Click(object sender, EventArgs e)
         {
+            g = this.CreateGraphics();
+            Brush blackBrush = new SolidBrush(Color.Black);
 
+            points[0] = new PointF(110, 5);
+            points[1] = new PointF(140, 80);
+            points[2] = new PointF(225, 80);
+            points[3] = new PointF(155, 125);
+            points[4] = new PointF(180, 200);
+            points[5] = new PointF(115, 160);
+            points[6] = new PointF(40, 200);
+            points[7] = new PointF(70, 130);
+            points[8] = new PointF(5, 80);
+            points[9] = new PointF(85, 80);
+
+            g.FillPolygon(blackBrush, points);
         }
 
         public void DrawStar(Pen starPen, float x, float y, float pixels)
         {
-
+            
 
         }
 
